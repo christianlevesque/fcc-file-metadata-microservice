f007da2e0ffbd153da8631d39b7f130ff5497b5a
Christian LeVesque <christian@christianlevesque.io>
2020-12-24 21:58:46 -0600

    added CORS support

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,30 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace file_metadata
 {
     public class Startup
     {
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCors(options =>
+			{
+				options.AddDefaultPolicy(policy =>
+				{
+					policy.WithOrigins("https://www.freecodecamp.org");
+					policy.WithMethods("POST");
+					policy.AllowAnyHeader();
+				});
+			});
+		}
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
-        }
+
+			app.UseCors();
+		}
     }
 }
